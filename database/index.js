293826ad3e65ebb0a6f18d6db51c395da70417f6
tran van phat <tranphat701@gmail.com>
2023-09-25 09:49:37 +0700

    case

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connect = () => {
   mongoose
-    .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+    .connect({ useNewUrlParser: true })
     .then(() => {
       console.log("database connect successfully !");
     })
